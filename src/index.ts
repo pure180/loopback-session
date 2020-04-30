e37e8f702152cbd8ad094c777a48638aa17248e6
Daniel Pfisterer <info@daniel-pfisterer.de>
2020-04-30 22:08:40 +0200

    add repository

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from './keys';
 export * from './models';
 export * from './providers';
+export * from './repositories';
 export * from './services';
 export * from './session.component';
 export * from './types';
-
