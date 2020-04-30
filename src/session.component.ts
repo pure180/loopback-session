e4a1e9809b97af90e077e307da80973ae5988540
Daniel Pfisterer <info@daniel-pfisterer.de>
2020-04-30 22:09:59 +0200

    Adjust component bindings

--- src/session.component.ts
+++ src/session.component.ts
@@ -1,37 +1,36 @@
-import { Application, bind, Binding, Component, ContextTags, CoreBindings, inject, ProviderMap } from '@loopback/core';
+import { bind, Binding, Component, ContextTags, ProviderMap, config } from '@loopback/core';
 import { CookieSerializeOptions } from 'cookie';
 
 import { SessionBindings } from './keys';
 import { SessionProvider } from './providers';
 import { SessionRepository } from './repositories';
 import { SessionStore } from './services';
-import { SessionOptions } from './types';
-import { SessionDataSource } from './datasources';
+import { SessionConfig } from './types';
+import { juggler } from '@loopback/repository';
 
 @bind({tags: {[ContextTags.KEY]: SessionBindings.COMPONENT}})
-export class SessionComponent implements Component {
+export class SessionComponent<DataSource extends juggler.DataSource> implements Component {
   providers: ProviderMap;
   
-  bindings: Binding<unknown>[] = [
-    Binding.bind(SessionBindings.DATA_SOURCE).toClass(SessionDataSource),
+  bindings: Binding<unknown>[]  = [
     Binding.bind(SessionBindings.MEMORY).toClass(SessionStore),
     Binding.bind(SessionBindings.REPOSITORY).toClass(SessionRepository),
   ];
 
   constructor(
-    @inject(CoreBindings.APPLICATION_INSTANCE) private readonly app: Application,
+    @config() sessionConfig: SessionConfig<DataSource>,
   ) {
     this.providers = {
       [SessionBindings.PROVIDER.key]: SessionProvider,
     };
-  }
-}
 
-export const registerSessionComponent = (app: Application, options: {
-  session: SessionOptions,
-  cookie?: CookieSerializeOptions,
-}) => {
-  app.component(SessionComponent)
-  app.bind(SessionBindings.COOKIE_OPTIONS.key).to(options.cookie || {});
-  app.bind(SessionBindings.OPTIONS.key).to(options.session);
+    console.dir(this)
+    console.dir(sessionConfig)
+
+    this.bindings.push(...[
+      Binding.bind<DataSource>(SessionBindings.DATA_SOURCE).toClass(sessionConfig.DataSource),
+      Binding.bind(SessionBindings.COOKIE_OPTIONS.key).to(sessionConfig.cookie || {}),
+      Binding.bind(SessionBindings.OPTIONS.key).to(sessionConfig.session),
+    ]);
+  }
 }
